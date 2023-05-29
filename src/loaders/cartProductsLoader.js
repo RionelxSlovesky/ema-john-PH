a6c970612ef1e388957b1191a269ce49bd2ca096
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-05-29 18:54:06 +0600

    Loading specific products instead of all
    Instead of loading all the products, now loading specific products in the cart to display

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,29 +1,40 @@
 import { getShoppingCart } from "../utilities/fakedb";
 
 const cartProductsLoader = async () => {
-    const loadedProducts = await fetch('http://localhost:5000/products');
-    const products = await loadedProducts.json();
+  // if cart data is in database, you have to use async await
+  const storedCart = getShoppingCart();
+  const ids = Object.keys(storedCart);
 
-    // if cart data is in database, you have to use async await
-    const storedCart = getShoppingCart();
-
-    const savedCart = [];
-
-    for (const id in storedCart) {
-        const addedProduct = products.find(pd => pd._id === id);
-        if (addedProduct) {
-            const quantity = storedCart[id];
-            addedProduct.quantity = quantity;
-            savedCart.push(addedProduct);
-        }
+  const loadedProducts = await fetch(
+    `http://localhost:5000/productsByIds`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(ids)
     }
+  );
+  const products = await loadedProducts.json();
+  console.log(products)
 
-    // if you need to send two things
-    // return [products, savedCart]
-    // another options
-    // return { products, cart: savedCart }
+  const savedCart = [];
 
-    return savedCart;
-}
+  for (const id in storedCart) {
+    const addedProduct = products.find((pd) => pd._id === id);
+    if (addedProduct) {
+      const quantity = storedCart[id];
+      addedProduct.quantity = quantity;
+      savedCart.push(addedProduct);
+    }
+  }
+
+  // if you need to send two things
+  // return [products, savedCart]
+  // another options
+  // return { products, cart: savedCart }
+
+  return savedCart;
+};
 
 export default cartProductsLoader;
